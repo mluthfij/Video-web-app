bcebc36f08afd95ea8edcf7fd97df1bbcd472617
mluthfij <luthfujuliansyah48@gmail.com>
2023-01-30 16:26:26 +0700

    add phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,22 +10,23 @@ class User < ApplicationRecord
          
          validates :username, presence: true, uniqueness: { case_sensitive: false }
          validate :validate_username
+         validate :validate_phone
+         
 
          attr_writer :login
 
          def login
-          @login || self.username || self.email
+          @login || self.username || self.email || self.phone_number
          end
 
         def self.find_for_database_authentication(warden_conditions)
           conditions = warden_conditions.dup
           if (login = conditions.delete(:login))
-            where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-          elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+            where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value OR lower(phone_number) = :value", { :value => login.downcase }]).first
+          elsif conditions.has_key?(:username) || conditions.has_key?(:email) || conditions.has_key?(:phone_number)
             where(conditions.to_h).first
           end
         end
-
 
         def validate_username
           if User.where(email: username).exists?
@@ -33,4 +34,9 @@ class User < ApplicationRecord
           end
         end
 
+        def validate_phone
+          if User.where(email: phone_number).exists?
+            errors.add(:phone_number, :invalid)
+          end
+        end
 end
