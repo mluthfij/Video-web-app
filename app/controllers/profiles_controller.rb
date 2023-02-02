6bee8d78323ffd8424e048ef03c63223c747fa75
mluthfij <luthfujuliansyah48@gmail.com>
2023-02-02 16:46:55 +0700

    edit view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,10 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
   def index
     @user = User.find_by_id(params[:id])
+    @videos = @user.videos.all
+  end
+  def show_profile
+    @user = User.find_by_id(params[:id])
+    @videos = @user.videos.all    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'profile/:id', to: 'profiles#index', as: :profile
+  get 'show_profile/:id', to: 'profiles#show_profile', as: :show_profile
   devise_for :users
   resources :videos
   root 'pages#home'
